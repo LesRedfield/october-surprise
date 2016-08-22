3c0c4a69259496127a4895fd3fdb30af6441034f
App Academy Student <les.redfield.dev@gmail.com>
2016-08-22 10:38:10 -0400

    Add slider logic

--- map.js
+++ map.js
@@ -26,6 +26,14 @@ uStates.draw("#statesvg", sampleData, tooltipHtml);
 d3.select(self.frameElement).style("height", "600px");
 
 $(function() {
+  new Dragdealer('overall-slider', {
+    animationCallback: function(x, y) {
+      $('#overall-slider .value').text(Math.round(x * 100));
+    }
+  });
+});
+
+$(function() {
   new Dragdealer('just-a-slider', {
     animationCallback: function(x, y) {
       $('#just-a-slider .value').text(Math.round(x * 100));
--- slider/script.js
+++ slider/script.js
@@ -1,7 +1,17 @@
+
+
+$(function() {
+  new Dragdealer('overall-slider', {
+    animationCallback: function(x, y) {
+      $('#overall-slider .value').text(Math.round(x * 100));
+    }
+  });
+});
+
 $(function() {
   new Dragdealer('just-a-slider', {
     animationCallback: function(x, y) {
       $('#just-a-slider .value').text(Math.round(x * 100));
     }
   });
-})
+});
